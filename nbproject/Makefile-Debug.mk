c4bc5ac7431711ad0b218b29beb5738e33f786a6
leyarotheconquerer <HazenJohnson@letu.edu>
2013-07-30 19:34:12 -0500

    Adding project files incase everything falls apart on me and I have to
    revert.

---
+++ nbproject/Makefile-Debug.mk
@@ -0,0 +1,224 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=Cygwin_4.x-Windows
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/GridBitmapWriter.o \
+	${OBJECTDIR}/MidpointGenerator.o \
+	${OBJECTDIR}/Grid.o \
+	${OBJECTDIR}/BitmapWriter.o
+
+# Test Directory
+TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
+
+# Test Files
+TESTFILES= \
+	${TESTDIR}/TestFiles/f1
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/midpoint2.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/midpoint2.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/midpoint2 ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/main.o: main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -g -I../../../../../lib/cppunit-1.12.1/include -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+
+${OBJECTDIR}/GridBitmapWriter.o: GridBitmapWriter.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -g -I../../../../../lib/cppunit-1.12.1/include -MMD -MP -MF $@.d -o ${OBJECTDIR}/GridBitmapWriter.o GridBitmapWriter.cpp
+
+${OBJECTDIR}/MidpointGenerator.o: MidpointGenerator.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -g -I../../../../../lib/cppunit-1.12.1/include -MMD -MP -MF $@.d -o ${OBJECTDIR}/MidpointGenerator.o MidpointGenerator.cpp
+
+${OBJECTDIR}/Grid.o: Grid.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -g -I../../../../../lib/cppunit-1.12.1/include -MMD -MP -MF $@.d -o ${OBJECTDIR}/Grid.o Grid.cpp
+
+${OBJECTDIR}/BitmapWriter.o: BitmapWriter.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -g -I../../../../../lib/cppunit-1.12.1/include -MMD -MP -MF $@.d -o ${OBJECTDIR}/BitmapWriter.o BitmapWriter.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Build Test Targets
+.build-tests-conf: .build-conf ${TESTFILES}
+${TESTDIR}/TestFiles/f1: ${TESTDIR}/test/GridIteratorDiaTest.o ${TESTDIR}/test/GridIteratorRectTest.o ${TESTDIR}/test/GridIteratorTest.o ${TESTDIR}/test/GridTest.o ${TESTDIR}/tests/Test.o ${OBJECTFILES:%.o=%_nomain.o}
+	${MKDIR} -p ${TESTDIR}/TestFiles
+	${LINK.cc}   -o ${TESTDIR}/TestFiles/f1 $^ ${LDLIBSOPTIONS} 
+
+
+${TESTDIR}/test/GridIteratorDiaTest.o: test/GridIteratorDiaTest.cpp 
+	${MKDIR} -p ${TESTDIR}/test
+	${RM} $@.d
+	$(COMPILE.cc) -g -I. -I../../../../../lib/cppunit-1.12.1/include -MMD -MP -MF $@.d -o ${TESTDIR}/test/GridIteratorDiaTest.o test/GridIteratorDiaTest.cpp
+
+
+${TESTDIR}/test/GridIteratorRectTest.o: test/GridIteratorRectTest.cpp 
+	${MKDIR} -p ${TESTDIR}/test
+	${RM} $@.d
+	$(COMPILE.cc) -g -I. -I../../../../../lib/cppunit-1.12.1/include -MMD -MP -MF $@.d -o ${TESTDIR}/test/GridIteratorRectTest.o test/GridIteratorRectTest.cpp
+
+
+${TESTDIR}/test/GridIteratorTest.o: test/GridIteratorTest.cpp 
+	${MKDIR} -p ${TESTDIR}/test
+	${RM} $@.d
+	$(COMPILE.cc) -g -I. -I../../../../../lib/cppunit-1.12.1/include -MMD -MP -MF $@.d -o ${TESTDIR}/test/GridIteratorTest.o test/GridIteratorTest.cpp
+
+
+${TESTDIR}/test/GridTest.o: test/GridTest.cpp 
+	${MKDIR} -p ${TESTDIR}/test
+	${RM} $@.d
+	$(COMPILE.cc) -g -I. -I../../../../../lib/cppunit-1.12.1/include -MMD -MP -MF $@.d -o ${TESTDIR}/test/GridTest.o test/GridTest.cpp
+
+
+${TESTDIR}/tests/Test.o: tests/Test.cpp 
+	${MKDIR} -p ${TESTDIR}/tests
+	${RM} $@.d
+	$(COMPILE.cc) -g -I. -I../../../../../lib/cppunit-1.12.1/include -MMD -MP -MF $@.d -o ${TESTDIR}/tests/Test.o tests/Test.cpp
+
+
+${OBJECTDIR}/main_nomain.o: ${OBJECTDIR}/main.o main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/main.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -I../../../../../lib/cppunit-1.12.1/include -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/main_nomain.o main.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/main.o ${OBJECTDIR}/main_nomain.o;\
+	fi
+
+${OBJECTDIR}/GridBitmapWriter_nomain.o: ${OBJECTDIR}/GridBitmapWriter.o GridBitmapWriter.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/GridBitmapWriter.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -I../../../../../lib/cppunit-1.12.1/include -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/GridBitmapWriter_nomain.o GridBitmapWriter.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/GridBitmapWriter.o ${OBJECTDIR}/GridBitmapWriter_nomain.o;\
+	fi
+
+${OBJECTDIR}/MidpointGenerator_nomain.o: ${OBJECTDIR}/MidpointGenerator.o MidpointGenerator.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/MidpointGenerator.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -I../../../../../lib/cppunit-1.12.1/include -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/MidpointGenerator_nomain.o MidpointGenerator.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/MidpointGenerator.o ${OBJECTDIR}/MidpointGenerator_nomain.o;\
+	fi
+
+${OBJECTDIR}/Grid_nomain.o: ${OBJECTDIR}/Grid.o Grid.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/Grid.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -I../../../../../lib/cppunit-1.12.1/include -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/Grid_nomain.o Grid.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/Grid.o ${OBJECTDIR}/Grid_nomain.o;\
+	fi
+
+${OBJECTDIR}/BitmapWriter_nomain.o: ${OBJECTDIR}/BitmapWriter.o BitmapWriter.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/BitmapWriter.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -I../../../../../lib/cppunit-1.12.1/include -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/BitmapWriter_nomain.o BitmapWriter.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/BitmapWriter.o ${OBJECTDIR}/BitmapWriter_nomain.o;\
+	fi
+
+# Run Test Targets
+.test-conf:
+	@if [ "${TEST}" = "" ]; \
+	then  \
+	    ${TESTDIR}/TestFiles/f1 || true; \
+	else  \
+	    ./${TEST} || true; \
+	fi
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/midpoint2.exe
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
